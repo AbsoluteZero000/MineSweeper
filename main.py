6c90540eec25fc9a8d72eddead4160760fa0ce54
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-06-23 10:04:56 +0300

    inital playable game

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class MineSweeper:
         self.height = height
         self.num_bombs = num_bombs
         self.grid = [[' ' for _ in range(width)] for _ in range(height)]
-
+        self.player_grid = [['X' for _ in range(width)] for _ in range(height)]
 
     def _place_bombs(self, x, y):
         placed_bombs = 0
@@ -32,28 +32,53 @@ class MineSweeper:
                     count += 1
         return count
 
+    def _reveal_adjacent_cells(self, x, y):
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < self.width and 0 <= ny < self.height and self.player_grid[ny][nx] == 'X' and self.grid[ny][nx] != 'B':
+                    self.player_grid[ny][nx] = self.grid[ny][nx]
+                    if self.grid[ny][nx] == 0:
+                        self._reveal_adjacent_cells(nx, ny)
+
     def display_grid(self):
+        for row in self.player_grid:
+            print(' '.join(str(cell) for cell in row))
+    def display_final_map(self):
         for row in self.grid:
             print(' '.join(str(cell) for cell in row))
-    
+    def _win(self):
+        cnt = 0
+        for row in self.player_grid:
+            if 'X' in row:
+                cnt+=1
+        return cnt == self.num_bombs
     def game_loop(self):
+        self.display_grid()
+
         x = get_valid_input("Enter the x-coordinate: ", 0, self.width - 1)
         y = get_valid_input("Enter the y-coordinate: ", 0, self.height - 1)
         self._place_bombs(x, y)
         self._calculate_adjacent_bombs()
+        self._reveal_adjacent_cells(x, y)
         self.display_grid()
+
 
         while True:
             x = get_valid_input("Enter the x-coordinate: ", 0, self.width - 1)
             y = get_valid_input("Enter the y-coordinate: ", 0, self.height - 1)
-            self.display_grid()
 
             if self.grid[y][x] == 'B':
                 print("YOU LOSE!")
-                self.display_grid()
+                self.display_final_map()
                 return
-            if self.grid[y][x] == 0:
-                self._reveal_adjacent_cells(x, y)
+            self._reveal_adjacent_cells(x, y)
+            self.display_grid()
+            if(self._win()):
+                print("YOU WIN!")
+                self.display_final_map()
+                return
+
 def get_valid_input(prompt, min_value, max_value=None):
     while True:
         try:
